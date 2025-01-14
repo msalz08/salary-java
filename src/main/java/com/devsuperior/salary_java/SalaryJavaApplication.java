963de1cb5f0b8c7e853494ec897185fbe48531a2
msalz08 <105805078+msalz08@users.noreply.github.com>
2025-01-14 16:33:55 -0300

    Add files via upload

---
+++ src/main/java/com/devsuperior/salary_java/SalaryJavaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.salary_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaryJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/salary_java/SalaryJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.salary_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
